109d1eded8d1134b96f8e9583a1f55692764945d
anthonyzaman0451@gmail.com <anthonyzaman0451@gmail.com>
2018-07-02 14:57:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}  
+function LogWhisper(string) {
+  console.log("HELLO")
+}
+function whisper(string) {
+  return string.toLowerCase()
+  function logShout(string) {
+  return "hello"
+}
+function sayHiToGrandma(string) {
+  console.log("hello")
+  return 
+}
