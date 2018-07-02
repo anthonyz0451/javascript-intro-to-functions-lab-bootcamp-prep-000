caeacc314711668beaf5e8de4a48b1518826d589
anthonyzaman0451@gmail.com <anthonyzaman0451@gmail.com>
2018-07-02 15:03:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,21 +4,9 @@ function sayhello() {
 function sayHELLO() {
   console.log('HELLO')
 }
-
-
-
 function shout(string) {
   return string.toUpperCase()
 }  
-function LogWhisper(string) {
-  console.log("HELLO")
-}
 function whisper(string) {
   return string.toLowerCase()
-  function logShout(string) {
-  return "hello"
-}
-function sayHiToGrandma(string) {
-  console.log("hello")
-  return 
 }
